4498f41ec8f6eef438df2ea6d2bdfad31ba14b7d
pery <50549920+peirongli@users.noreply.github.com>
2024-11-02 21:49:06 +0800

    Update part3-哈希表.py

--- part3-哈希表.py
+++ part3-哈希表.py
@@ -76,8 +76,139 @@ class Solution:
             seen.add(num)
 
 #哈希表4 - 四数相加II (LeetCode 454):
-
+#用空间换时间, 把原本O(n^4)的问题转化为2个O(n^2)的问题
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        n = len(nums1)
+        ab = {}
+        result = 0
+        for i in range(n):
+            for j in range(n):
+                if nums1[i] + nums2[j] not in ab.keys():
+                    ab.update({nums1[i] + nums2[j]: 1})
+                else:
+                    ab[nums1[i] + nums2[j]] += 1
+        for i in range(n):
+            for j in range(n):
+                if 0 - nums3[i] - nums4[j] in ab.keys():
+                    result += ab[0 - nums3[i] - nums4[j]]
+        return result
 
 #哈希表5 - 三数之和 (LeetCode 15):
+#初版: 有去重复, 但是超时了(通过了255/313个测试用例)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        result = []
+        dc = {}
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                else:
+                    dc.update({(i,j): nums[i] + nums[j]})
+        for k in range(n):
+            for key in dc.keys():
+                idx_i, idx_j = key
+                val = dc[key]
+                if 0 - nums[k] == val:
+                    if k != idx_i and k != idx_j:
+                        temp_list = [nums[idx_i], nums[idx_j], nums[k]]
+                        temp_list.sort()
+                        if temp_list not in result:
+                            result.append(temp_list)
+        return result
+#改进版: 双指针(容易理解, 且去重不容易写错)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        result = []
+        for i in range(n):
+            #如果第一个元素已经大于0(target)，不需要进一步检查
+            if nums[i] > 0:
+                return result
+            #第1个元素去重
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+            while right > left:
+                if nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                else: #sum = 0(target)
+                    result.append([nums[i], nums[left], nums[right]])
+                    
+                    #第2个元素去重
+                    while right > left and nums[left] == nums[left + 1]:
+                        left += 1
+                    #第3个元素去重        
+                    while right > left and nums[right] == nums[right - 1]:
+                        right -= 1
+                       
+                    left += 1
+                    right -= 1
+                
+        return result
 
 #哈希表6 - 四数之和 (LeetCode 18):
+#类似题5的初版, 超时(通过221/294个测试用例)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        result = []
+        dc = {}
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                else:
+                    dc.update({(i,j): nums[i] + nums[j]})
+        for k in range(n):
+            for m in range(n):
+                for key in dc.keys():
+                    idx_i, idx_j = key
+                    val = dc[key]
+                    if target - nums[k] - nums[m] == val:
+                        if k != idx_i and k != idx_j and k != m and m != idx_i and m != idx_j:
+                            temp_list = [nums[idx_i], nums[idx_j], nums[k], nums[m]]
+                            temp_list.sort()
+                            if temp_list not in result:
+                                result.append(temp_list)
+        return result
+#双指针(格式和题5基本一样, 只是多套了一层循环; 依此写法, 五数、六数等之和也可以解决, 需要O(n^(N-1))的时间复杂度)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]: 
+        n = len(nums)
+        nums.sort()
+        result = []
+        for i in range(n):
+            #第1个元素去重
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i+1, n):
+                #第2个元素去重
+                if j > i+1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = n - 1
+                while right > left:
+                    if nums[j] + nums[left] + nums[right] > target - nums[i]:
+                        right -= 1
+                    elif nums[j] + nums[left] + nums[right] < target - nums[i]:
+                        left += 1
+                    else:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        #第3个元素去重
+                        while right > left and nums[left] == nums[left + 1]:
+                            left += 1
+                        #第3个元素去重        
+                        while right > left and nums[right] == nums[right - 1]:
+                            right -= 1
+                       
+                        left += 1
+                        right -= 1
+        return result
