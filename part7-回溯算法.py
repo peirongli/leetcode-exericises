fe64dd5b6e044c6213691268d3960b69c9a5fb06
pery <50549920+peirongli@users.noreply.github.com>
2025-01-20 20:28:13 +0800

    Update part7-回溯算法.py

--- part7-回溯算法.py
+++ part7-回溯算法.py
@@ -175,15 +175,143 @@ class Solution:
 
 #part2 分割
 #题7 - 分割回文串(leetcode 131)
+#思路不难理解, 实际细节有点复杂
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        self.backtracking(s, 0, [], result)
+        return result
+
+    def backtracking(self, s, start_index, path, result):
+        if start_index == len(s):
+            result.append(path[:])
+            return
+        
+        for i in range(start_index, len(s)):
+            if self.is_palindrome(s, start_index, i):
+                path.append(s[start_index:i+1])
+                self.backtracking(s, i+1, path, result)
+                path.pop()
+
+    def is_palindrome(self, s: str, start: int, end: int) -> bool:#用双指针的好处: 不用每次都切片处理
+        i: int = start        
+        j: int = end
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
 
 #题8 - 复原IP地址(leetcode 93)
+#改一下上题的判断函数就行, 本质一样
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        result = []
+        self.backtracking(s, 0, [], result)
+        return result
+
+    def backtracking(self, s, start_index, path, result):
+        if len(path) > 4:
+            return
+        if start_index == len(s) and len(path) == 4:
+            temp_addr = '.'.join(path)
+            result.append(temp_addr)
+            return
+        
+        for i in range(start_index, len(s)):
+            if self.isVaild(s, start_index, i):
+                path.append(s[start_index:i+1])
+                self.backtracking(s, i+1, path, result)
+                path.pop()
+
+    def isVaild(self, s: str, start: int, end: int) -> bool:
+        temp_str = s[start:end+1]
+        if s[start] == '0' and len(temp_str) > 1:
+            return False
+        if int(temp_str) < 0 or int(temp_str) > 255:
+            return False
+        return True
 
 #part3 子集
 #题9 - 子集(leetcode 78)
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.backtrack(nums, 0, [], result)
+        return result
+    def backtrack(self, nums, start, path, result):
+        #不需要终止条件
+        result.append(path[:])
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            self.backtrack(nums, i+1, path, result)
+            path.pop()
 
 #题10 - 子集2(leetcode 90)
+#按常规的想法应该要用used数组去重, 但貌似排序之后再加一行判断也行?
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        self.backtrack(nums, 0, [], result)
+        return result
+    def backtrack(self, nums, start, path, result):
+        temp = path[:]
+        if temp not in result:
+            result.append(temp)
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            self.backtrack(nums, i+1, path, result)
+            path.pop()
 
 #题11 - 递增子序列(leetcode 491)
+#笨方法, 很极限没超时
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.backtrack(nums, 0, [], result)
+        return result
+
+    def backtrack(self, nums, start, path, result):
+        if len(path) > 1 and self.isAscending(path):
+            temp = path[:]
+            if temp not in result:
+                result.append(temp)
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            self.backtrack(nums, i+1, path, result)
+            path.pop()
+            
+    def isAscending(self, numbers):
+        for i in range(len(numbers)-1):
+            if numbers[i+1] < numbers[i]:
+                return False
+        return True
+#set去重:
+class Solution:
+    def findSubsequences(self, nums):
+        result = []
+        self.backtracking(nums, 0, [], result)
+        return result
+    
+    def backtracking(self, nums, startIndex, path, result):
+        if len(path) > 1:
+            result.append(path[:])
+            # 注意这里不要加return，要取树上的节点
+        
+        uset = set()  # 使用集合对本层元素进行去重
+        for i in range(startIndex, len(nums)):
+            if (path and nums[i] < path[-1]) or nums[i] in uset:
+                continue
+            
+            uset.add(nums[i])  # 记录这个元素在本层用过了，本层后面不能再用了
+            path.append(nums[i])
+            self.backtracking(nums, i + 1, path, result)
+            path.pop()
 
 #part4 排列
 #题12 - 全排列(leetcode 46)
